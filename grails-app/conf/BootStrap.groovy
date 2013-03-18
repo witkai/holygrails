9b3a84e4dfe6b5b58bbc8130f6fb147c0dd47b98
Kai <witkai@gmail.com>
2013-03-18 15:06:08 -0400

    Initialize Quest and Task

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,17 @@
+import org.junit.internal.runners.statements.FailOnTimeout;
+
+import cc.*
+
 class BootStrap {
 
     def init = { servletContext ->
+		Quest q1 = new Quest(name:"Seek the grail")
+			.addToTasks(name:"Buy horse", completed:true)
+			.addToTasks(name:"Defeat the Black Knight")
+			.addToTasks(name:"Answer the bridgekeeper", priority:4)
+			.save(failOnError:true)
     }
+	
     def destroy = {
     }
 }
